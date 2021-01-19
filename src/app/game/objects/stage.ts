85c099054a791818a1218f6b69fd960d74638725
Jon <75381727+softm-joncanizales@users.noreply.github.com>
2021-01-19 15:12:54 -0600

    + Added Game Objects
    +Stage
    + Ball (Example)

---
+++ src/app/game/objects/stage.ts
@@ -0,0 +1,51 @@
+export class Stage extends Phaser.GameObjects.Image{
+
+    private _scene;
+    private image;
+    public label = "";
+    private status:number = 0;
+
+    constructor(scene, x, y, value, texture){
+
+        super(scene, x, y, texture);
+
+         this._scene = scene;
+         this.setInteractive();
+ 
+         this.on('pointerover', function (pointer) {
+             switch(this.status){
+                 case -1: this.setTint(0xbbbbbb); break;
+                 default: this.setTint(0x00ffff); break;
+             }                   
+         });
+ 
+         this.on('pointerout', function (pointer) {
+             this.clearTint();
+         });
+
+    }
+
+    setLabel(value:number | string){
+        this.label = this._scene.add.text(
+            this.x -7,
+            this.y - 25 ,
+            value,
+            {
+              font: '24px Arial',
+              color: '#000000',
+            }
+        );    
+    }
+
+    setStatus(value:number){
+        this.status = value;
+        this.drawStatus();
+    }
+
+    private drawStatus(){
+        this.setFrame(this.status + 1);
+    }
+ 
+    
+ 
+ }
